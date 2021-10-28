ff5a4aeb05cd82844385ba2dfac88ce3781e2de5
Christian Andolf <christian.andolf@bonniernews.se>
2021-10-28 16:46:15 +0200

    Bumped to node 14. Added mocha and eslint configs. Local redis used for testing purposes. Refactored code to use mostly async/await.

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
 "use strict";
-const DEFAULT_RETRY_MS = 2000;
+
 const EventEmitter = require("events");
-const util = require("util");
-const redis = require("redis");
+const Redis = require("ioredis");
 
-// bluebird polyfill
-Promise.map = function(iterable, mapper, options) {
-  options = options || {};
-  let concurrency = options.concurrency || Infinity;
-
-  let index = 0;
-  const results = [];
-  const iterator = iterable[Symbol.iterator]();
-  const promises = [];
-
-  while (concurrency-- > 0) {
-    const promise = wrappedMapper();
-    if (promise) promises.push(promise);
-    else break;
-  }
-
-  return Promise.all(promises).then(() => results);
-
-  function wrappedMapper() {
-    const next = iterator.next();
-    if (next.done) return null;
-    const i = index++;
-    const mapped = mapper(next.value, i);
-    return Promise.resolve(mapped).then((resolved) => {
-      results[i] = resolved;
-      return wrappedMapper();
-    });
-  }
-};
+const DEFAULT_RETRY_MS = 2000;
 
 function serialize(value) {
   if (value === null) {
@@ -64,128 +35,118 @@ function deserializeAll(values) {
   return values.map(deserialize);
 }
 
-function RedisCache(options) {
-  options = options || {};
-  EventEmitter.call(this);
+class RedisCache extends EventEmitter {
+  constructor(options = {}) {
+    super(options);
 
-  const self = this;
-  options.retry_strategy =
-    options.retry_strategy ||
-    function() {
-      return DEFAULT_RETRY_MS;
-    };
-  const isNumberRegEx = /^\d+$/;
+    options.retryStrategy = options.retryStrategy || (() => DEFAULT_RETRY_MS);
+    this.options = options;
+    const isNumberRegEx = /^\d+$/;
 
-  if (options.maxAge && options.maxAge !== "number" && !isNumberRegEx.test(options.maxAge)) {
-    throw new Error(`Unparsable maxAge option: '${options.maxAge}'`);
+    if (options.maxAge && options.maxAge !== "number" && !isNumberRegEx.test(options.maxAge)) {
+      throw new Error(`Unparsable maxAge option: '${options.maxAge}'`);
+    }
+    this.poolResolveTimeMs = options.poolTime;
+    this.resolveGetPoolTimer = false;
+    this.getPool = [];
+    this.client = new Redis(options);
+    this.client.on("error", (err) => {
+      this.emit("error", err);
+    });
   }
-  this.poolResolveTimeMs = options.poolTime;
-  this.resolveGetPoolTimer = false;
-  this.getPool = [];
-  this.client = redis.createClient(options);
-  this.client.on("error", function(err) {
-    self.emit("error", err);
-  });
 
-  this.client.getAsync = util.promisify(this.client.get);
-  this.client.getAllAsync = util.promisify(this.client.mget);
-  this.client.setexAsync = util.promisify(this.client.setex);
-  this.client.setAsync = util.promisify(this.client.set);
-  this.client.delAsync = util.promisify(this.client.del);
-  this.client.keysAsync = util.promisify(this.client.keys);
-
-  this.get = function(key) {
+  async get(key) {
     if (this.poolResolveTimeMs && this.poolResolveTimeMs > 0) {
       return this.addGetToPool(key);
     } else {
-      return this.client.getAsync(key).then(deserialize);
+      const value = await this.client.get(key);
+      return deserialize(value);
     }
-  };
+  }
 
-  this.addGetToPool = function(key) {
-    return new Promise(function(resolve, reject) {
+  async addGetToPool(key) {
+    return new Promise((resolve, reject) => {
       const getVO = {
-        key: key,
-        resolve: resolve,
-        reject: reject
+        key,
+        resolve,
+        reject
       };
-      self.getPool.push(getVO);
-      if (!self.resolveGetPoolTimer) {
-        if (self.poolResolveTimeMs === 1) {
-          self.resolveGetPoolTimer = true;
-          setImmediate(self.resolveGetPool);
+      this.getPool.push(getVO);
+      if (!this.resolveGetPoolTimer) {
+        if (this.poolResolveTimeMs === 1) {
+          this.resolveGetPoolTimer = true;
+          setImmediate(this.resolveGetPool);
         } else {
-          self.resolveGetPoolTimer = setTimeout(
-            self.resolveGetPool,
-            self.poolResolveTimeMs
+          this.resolveGetPoolTimer = setTimeout(
+            this.resolveGetPool,
+            this.poolResolveTimeMs
           );
         }
       }
     });
-  };
+  }
 
-  this.resolveGetPool = function() {
-    self.resolveGetPoolTimer = false;
-    const localGetPool = self.getPool.slice(0);
-    self.getPool = [];
-    const keys = localGetPool.map(function(getVO) {
-      return getVO.key;
-    });
-    self
+  resolveGetPool() {
+    this.resolveGetPoolTimer = false;
+    const localGetPool = this.getPool.slice(0);
+    this.getPool = [];
+    const keys = localGetPool.map((getVO) => getVO.key);
+    this
       .getAll(keys)
-      .then(function(serializedItems) {
-        localGetPool.forEach(function(getVO, index) {
+      .then((serializedItems) => {
+        localGetPool.forEach((getVO, index) => {
           getVO.resolve(serializedItems[index]);
         });
       })
-      .catch(function(err) {
-        localGetPool.forEach(function(getVO) {
+      .catch((err) => {
+        localGetPool.forEach((getVO) => {
           getVO.reject(err);
         });
       });
-  };
+  }
 
-  this.getAll = function(keys) {
-    return this.client.getAllAsync(keys).then(deserializeAll);
-  };
-  this.peek = function(key) {
+  async getAll(keys) {
+    const values = await this.client.mget(keys);
+    return deserializeAll(values);
+  }
+
+  async peek(key) {
     return this.get(key);
-  };
-  this.has = function(key) {
-    return this.client.getAsync(key).then(function(value) {
-      return value !== null;
-    });
-  };
-  this.set = function(key, value, maxAge) {
+  }
+
+  async has(key) {
+    const value = await this.client.get(key);
+    return value !== null;
+  }
+
+  async set(key, value, maxAge) {
     const hasTtl = typeof maxAge === "number";
-    const hasDefaultMaxAge = options && options.maxAge;
+    const hasDefaultMaxAge = this.options.maxAge;
 
     if (hasTtl && maxAge <= 0) {
       return Promise.resolve();
     } else if (hasTtl && maxAge > 0) {
-      return this.client.setexAsync(
+      return this.client.setex(
         key,
         Math.round(maxAge / 1000),
         serialize(value)
       );
     } else if (hasDefaultMaxAge) {
-      return this.set(key, value, Number(options.maxAge));
+      return this.set(key, value, Number(this.options.maxAge));
     } else {
-      return this.client.setAsync(key, serialize(value));
+      return this.client.set(key, serialize(value));
     }
-  };
-  this.del = function(key) {
-    return this.client.delAsync(key);
-  };
-  this.reset = function() {
-    return this.client.keysAsync("*").then(function(keys) {
-      return Promise.map(keys, function(key) {
-        return self.del(key);
-      });
-    });
-  };
-}
+  }
 
-util.inherits(RedisCache, EventEmitter);
+  async del(key) {
+    return this.client.del(key);
+  }
+
+  async reset() {
+    const keys = await this.client.keys("*");
+    const deleters = keys.map((key) => this.del(key));
+    return Promise.all(deleters);
+  }
+}
 
 module.exports = RedisCache;
--- test/index.js
+++ test/index.js
@@ -1,372 +1,212 @@
 "use strict";
-require("chai").should();
 
-var redisStub = {
-  createClient: function(options) {
-    var cache = (options || {}).cache || {};
-    var onCallbacks = {};
-    return {
-      cache: cache,
-      options: options,
-      get: function(key, callback) {
-        if (key === "error") {
-          return callback(new Error("redis error"));
-        }
-        if (cache.hasOwnProperty(key)) {
-          callback(null, cache[key].value);
-        } else {
-          callback(null, null);
-        }
-      },
-      mget: function(keys, callback) {
-        if (keys === "error") {
-          return callback(new Error("redis error"));
-        }
-        const results = [];
-        keys.forEach(function(key) {
-          if (cache.hasOwnProperty(key)) {
-            results.push(cache[key].value);
-          } else {
-            results.push(null);
-          }
-        });
-        callback(null, results);
-      },
-      setex: function(key, ttl, value, callback) {
-        if (key === "error") {
-          return callback(new Error("redis error"));
-        }
-        cache[key] = {
-          value: value,
-          ttl: ttl
-        };
-        callback(null, null);
-      },
-      set: function(key, value, callback) {
-        if (key === "error") {
-          return callback(new Error("redis error"));
-        }
-        cache[key] = {
-          value: value,
-          ttl: null
-        };
-        callback(null, null);
-      },
-      del: function(key, callback) {
-        delete cache[key];
-        callback();
-      },
-      keys: function(filter, callback) {
-        callback(null, Object.keys(cache));
-      },
-      on: function(event, callback) {
-        if (!onCallbacks[event]) {
-          onCallbacks[event] = [callback];
-        } else {
-          onCallbacks[event].push(callback);
-        }
-      },
-      emit: function(event, err) {
-        if (onCallbacks[event]) {
-          for (var i = 0; i < onCallbacks[event].length; i++) {
-            onCallbacks[event][i](err);
-          }
-        }
-      }
-    };
-  }
-};
-var proxyquire = require("proxyquire"),
-  RedisCache = proxyquire("../", { redis: redisStub }),
-  assert = require("assert");
+const RedisCache = require("../");
+const Redis = require("ioredis");
 
-describe("RedisCache", function() {
-  it("should pass options to redis client", function() {
-    var options = {
-      host: "127.0.0.1",
-      port: 6379,
-      retry_strategy: function() {}
-    };
-    var target = new RedisCache(options);
-    target.client.options.should.eql(options);
+describe("RedisCache", () => {
+  let client;
+  before(async () => {
+    client = new Redis();
+  });
+  beforeEach(() => {
+    return client.flushdb();
+  });
+  after(() => {
+    return client.disconnect();
   });
 
-  it("should add retry_stategy option if none specified", function() {
-    var options = {
+  it("should pass options to redis client", () => {
+    const options = {
+      host: "127.0.0.1",
+      port: 6379,
+      retryStrategy: () => {}
+    };
+    const target = new RedisCache(options);
+    expect(target.client.options).to.include(options);
+  });
+
+  it("should add retryStategy option if none specified", () => {
+    const options = {
       host: "127.0.0.1",
       port: 6379
     };
-    var target = new RedisCache(options);
-    target.client.options.should.have.property("retry_strategy");
-    target.client.options.retry_strategy().should.eql(2000);
+    const target = new RedisCache(options);
+    expect(target.client.options).to.have.property("retryStrategy");
+    expect(target.client.options.retryStrategy()).to.equal(2000);
   });
 
-  it("should get the value from redis as json", function(done) {
-    var options = {
-      cache: {
-        key: {
-          value: '"value"'
-        }
-      }
-    };
-    var target = new RedisCache(options);
-    target.get("key").then(function(value) {
-      value.should.equal("value");
-      done();
-    });
+  it("should get the value from redis as json", async () => {
+    await client.set("key", JSON.stringify("value"));
+
+    const target = new RedisCache();
+    const value = await target.get("key");
+    expect(value).to.equal("value");
   });
 
-  it('should return null if value in redis is "null"', function(done) {
-    var options = {
-      cache: {
-        key: {
-          value: "null"
-        }
-      }
-    };
-    var target = new RedisCache(options);
-    target.get("key").then(function(value) {
-      assert(value === null);
-      done();
-    });
+  it("should return null if value in redis is \"null\"", async () => {
+    await client.set("key", "null");
+
+    const target = new RedisCache();
+    const value = await target.get("key");
+    expect(value).to.equal(null);
   });
 
   // undefined means "not found" in contract with exp-asynccache so we cannot return it.
-  it('should return "undefined" if value in redis is "undefined"', function(done) {
-    var options = {
-      cache: {
-        key: {
-          value: "undefined"
-        }
-      }
-    };
-    var target = new RedisCache(options);
-    target.get("key").then(function(value) {
-      assert(value === "undefined");
-      done();
-    });
+  it("should return \"undefined\" if value in redis is \"undefined\"", async () => {
+    await client.set("key", "undefined");
+
+    const target = new RedisCache();
+    const value = await target.get("key");
+    expect(value).to.equal("undefined");
   });
 
-  it("should emit client errors", function(done) {
-    var target = new RedisCache();
-    target.on("error", function(err) {
-      assert(err);
-      err.message.should.equal("error");
-      done();
+  it("should emit client errors", async () => {
+    const target = new RedisCache();
+    target.on("error", (err) => {
+      expect(err).to.be.instanceOf(Error);
+      expect(err.message).to.equal("error");
     });
     target.client.emit("error", new Error("error"));
   });
 
-  it("should delete the key from redis", function(done) {
-    var options = {
-      cache: {
-        key: {
-          value: '"value"'
-        }
-      }
-    };
-    var target = new RedisCache(options);
-    target.del("key").then(function() {
-      target.has("key").then(function(value) {
-        value.should.equal(false);
-        done();
-      });
-    });
+  it("should delete the key from redis", async () => {
+    await client.set("key", JSON.stringify("value"));
+
+    const target = new RedisCache();
+    await target.del("key");
+    const value = await target.has("key");
+    expect(value).to.equal(false);
   });
 
-  it("should delete all keys from redis when resetting", function(done) {
-    var options = {
-      cache: {
-        key: {
-          value: '"value"'
-        },
-        key2: {
-          value: '"value"'
-        }
-      }
-    };
-    var target = new RedisCache(options);
-    target.reset().then(function() {
-      target.has("key").then(function(value) {
-        value.should.equal(false);
-        target.has("key2").then(function(value) {
-          value.should.equal(false);
-          done();
-        });
-      });
-    });
+  it("should delete all keys from redis when resetting", async () => {
+    await client.set("key", JSON.stringify("value"));
+    await client.set("key2", JSON.stringify("value"));
+
+    const target = new RedisCache();
+    await target.reset();
+
+    let value = await target.has("key");
+    expect(value).to.equal(false);
+
+    value = await target.has("key2");
+    expect(value).to.equal(false);
   });
 
-  it("should peek the value from redis as json", function(done) {
-    var options = {
-      cache: {
-        key: {
-          value: '"value"'
-        }
-      }
-    };
-    var target = new RedisCache(options);
-    target.peek("key").then(function(value) {
-      value.should.equal("value");
-      done();
-    });
+  it("should peek the value from redis as json", async () => {
+    await client.set("key", JSON.stringify("value"));
+
+    const target = new RedisCache();
+    const value = await target.peek("key");
+    expect(value).to.equal("value");
   });
 
-  it("should have the key if it is set", function(done) {
-    var options = {
-      cache: {
-        key: {
-          value: '"value"'
-        }
-      }
-    };
-    var target = new RedisCache(options);
-    target.has("key").then(function(value) {
-      value.should.equal(true);
-      done();
-    });
+  it("should have the key if it is set", async () => {
+    await client.set("key", JSON.stringify("value"));
+
+    const target = new RedisCache();
+    const value = await target.has("key");
+    expect(value).to.equal(true);
   });
 
-  it('should have the key if it is set to "null"', function(done) {
-    var options = {
-      cache: {
-        key: {
-          value: "null"
-        }
-      }
-    };
-    var target = new RedisCache(options);
-    target.has("key").then(function(value) {
-      value.should.equal(true);
-      done();
-    });
+  it("should have the key if it is set to \"null\"", async () => {
+    await client.set("key", "null");
+
+    const target = new RedisCache();
+    const value = await target.has("key");
+    expect(value).to.equal(true);
   });
 
-  it('should have the key if it is set to "undefined"', function(done) {
-    var options = {
-      cache: {
-        key: {
-          value: "undefined"
-        }
-      }
-    };
-    var target = new RedisCache(options);
-    target.has("key").then(function(value) {
-      value.should.equal(true);
-      done();
-    });
+  it("should have the key if it is set to \"undefined\"", async () => {
+    await client.set("key", "undefined");
+
+    const target = new RedisCache();
+    const value = await target.has("key");
+    expect(value).to.equal(true);
   });
 
-  it("should not have the key if it is missing", function(done) {
-    var target = new RedisCache();
-    target.has("key").then(function(value) {
-      value.should.equal(false);
-      done();
-    });
+  it("should not have the key if it is missing", async () => {
+    const target = new RedisCache();
+    const value = await target.has("key");
+    expect(value).to.equal(false);
   });
 
-  it("should return undefined if key is missing", function(done) {
-    var target = new RedisCache();
-    target.get("key").then(function(value) {
-      assert(value === undefined);
-      done();
-    });
+  it("should return undefined if key is missing", async () => {
+    const target = new RedisCache();
+    const value = await target.get("key");
+    expect(value).to.equal(undefined);
   });
 
-  it("should reject the promise on get error from redis", function(done) {
-    var target = new RedisCache();
-    target.get("error").then(done, function() {
-      done();
-    });
+  it("should set the value in redis with ttl in nearest seconds", async () => {
+    const target = new RedisCache();
+    await target.set("key", "value", 1500);
+
+    const ttl = await client.ttl("key");
+    expect(ttl).to.be.closeTo(2, 0.2);
   });
 
-  it("should set the value in redis with ttl in nearest seconds", function(done) {
-    var target = new RedisCache();
-    target.set("key", "value", 1500).then(function() {
-      target.client.cache.key.ttl.should.equal(2);
-      done();
-    });
+  it("should not set the value in redis if ttl is less than 1", async () => {
+    const target = new RedisCache();
+    await target.set("key", "value", 0);
+
+    const value = await client.get("key");
+    expect(value).to.equal(null);
   });
 
-  it("should not set the value in redis if ttl is less than 1", function(done) {
-    var target = new RedisCache();
-    target.set("key", "value", 0).then(function() {
-      assert(target.client.cache.key === undefined);
-      done();
-    });
+  it("should set the value in redis permanently if without ttl", async () => {
+    const target = new RedisCache();
+    await target.set("key", "value");
+
+    const ttl = await client.ttl("key");
+    expect(ttl).to.equal(-1);
   });
 
-  it("should set the value in redis permanently if without ttl", function(done) {
-    var target = new RedisCache();
-    target.set("key", "value").then(function() {
-      assert(target.client.cache.key.ttl === null);
-      done();
-    });
+  it("should set the value in redis permanently if maxAge is empty string", async () => {
+    const target = new RedisCache({maxAge: ""});
+    await target.set("key", "value");
+
+    const ttl = await client.ttl("key");
+    expect(ttl).to.equal(-1);
   });
 
-  it("should set the value in redis permanently if maxAge is empty string", function(done) {
-    var target = new RedisCache({ maxAge: "" });
-    target.set("key", "value");
+  it("should set the value in redis with a ttl if given in constructor options", async () => {
+    const target = new RedisCache({maxAge: 1000});
+    await target.set("key", "value");
 
-    assert(target.client.cache.key.ttl === null);
-    done();
+    const ttl = await client.ttl("key");
+    expect(ttl).to.equal(1);
   });
 
-  it("should set the value in redis with a ttl if given in constructor options", function(done) {
-    var target = new RedisCache({ maxAge: 1000 });
-    target.set("key", "value").then(function() {
-      target.client.cache.key.ttl.should.equal(1);
-      done();
-    });
+  it("should set the value in redis with a specifically given ttl that overrides global default values", async () => {
+    const target = new RedisCache({maxAge: 1000});
+    await target.set("key", "value", 2000);
+
+    const ttl = await client.ttl("key");
+    expect(ttl).to.equal(2);
   });
 
-  it("should set the value in redis with a specifically given ttl that overrides global default values", function(done) {
-    var target = new RedisCache({ maxAge: 1000 });
-    target.set("key", "value", 2000).then(function() {
-      target.client.cache.key.ttl.should.equal(2);
-      done();
-    });
+  it("should set the value in redis with a ttl if given as a parsable number in a string", async () => {
+    const target = new RedisCache({maxAge: "1000"});
+    await target.set("key", "value");
+
+    const ttl = await client.ttl("key");
+    expect(ttl).to.equal(1);
   });
 
-  it("should set the value in redis with a ttl if given as a parsable number in a string", function(done) {
-    var target = new RedisCache({ maxAge: "1000" });
-    target.set("key", "value").then(function() {
-      target.client.cache.key.ttl.should.equal(1);
-      done();
-    });
+  it("should set the value in redis without a ttl if default options was given as an empty string", async () => {
+    const target = new RedisCache({maxAge: ""});
+    await target.set("key", "value");
+
+    const ttl = await client.ttl("key");
+    expect(ttl).to.equal(-1);
   });
 
-  it("should set the value in redis without a ttl if default options was given as an empty string", done => {
-    var target = new RedisCache({ maxAge: "" });
-    target.set("key", "value").then(function() {
-      assert(target.client.cache.key.ttl === null);
-      done();
-    });
-  });
-
-  ["unknown", "Infinity", "-Infinity", " ", {}, [], Infinity, Math.random].map(input => {
-    it(`should throw on initialization if maxAge is '${input}'`, function(done) {
+  ["unknown", "Infinity", "-Infinity", " ", {}, [], Infinity, Math.random].map((input) => {
+    it(`should throw on initialization if maxAge is '${input}'`, async () => {
       try {
-        new RedisCache({ maxAge: input });
+        return new RedisCache({maxAge: input});
       } catch (error) {
-        error.message.should.equal(`Unparsable maxAge option: '${input}'`);
-        done();
+        expect(error.message).to.equal(`Unparsable maxAge option: '${input}'`);
       }
-    });
-  });
-
-  it("should set the value in redis as json", function(done) {
-    var target = new RedisCache();
-    target.set("key", { field: "value" }, 1500).then(function() {
-      target.client.cache.key.value.should.equal('{"field":"value"}');
-      done();
-    });
-  });
-
-  it("should reject the promise on set error from redis", function(done) {
-    var target = new RedisCache();
-    target.set("error", "value", 1500).then(done, function() {
-      done();
     });
   });
 });
